b4786e587d9313738ef270f2324346c534701433
paoloricciuti <ricciutipaolo@gmail.com>
2024-03-10 21:25:42 +0100

    chore: add health check + fix send polls

--- cron/send_polls.js
+++ cron/send_polls.js
@@ -5,61 +5,78 @@ const { Op, QueryTypes } = require('sequelize');
 const { sequelize } = require('../models');
 
 const base64ToString = (base64) => {
-    return Buffer.from(base64, "base64").toString("ascii");
+	return Buffer.from(base64, 'base64').toString('ascii');
 };
 
 const shuffle = (array) => {
-    const retval = [];
-    const hat = [...array];
-    for (let i = 0; i < array.length; i++) {
-
-        const random = Math.floor(Math.random() * hat.length);
-        retval.push(hat.splice(random, 1)[0]);
-    }
-    return retval;
+	const retval = [];
+	const hat = [...array];
+	for (let i = 0; i < array.length; i++) {
+		const random = Math.floor(Math.random() * hat.length);
+		retval.push(hat.splice(random, 1)[0]);
+	}
+	return retval;
 };
 
 const exec = async () => {
-    const { Chat, Poll } = db;
-    const hour = new Date().getHours();
-    const registeredChats = await sequelize.query('SELECT id, chat, active, hours, locale, created_at AS createdAt, updated_at AS updatedAt FROM chats AS Chat WHERE Chat.active = true AND Chat.hours LIKE CONCAT("%$",IF($1+Chat.locale<0, (24+($1+Chat.locale)), (($1+Chat.locale) % 24)),"$%")', {
-        bind: [hour],
-        type: QueryTypes.SELECT,
-        model: Chat,
-        mapToModel: true,
-    });
-    const res = await fetch(`https://opentdb.com/api.php?amount=${registeredChats.length}&type=multiple&encode=base64`);
-    const toReturn = [];
-    if (res.ok) {
-        const { results: questions } = await res.json();
-        let i = 0;
-        for (let chat of registeredChats) {
-            const data = questions[i];
-            const options = shuffle([...data.incorrect_answers, data.correct_answer]);
-            const sentPoll = await utils.sendPoll({
-                chat_id: chat.id,
-                question: base64ToString(data.question),
-                options: options.map(base64ToString),
-                is_anonymous: false,
-                type: "quiz",
-                correct_option_id: options.indexOf(data.correct_answer),
-                open_period: 600,
-            });
-            console.log(sentPoll);
-            try {
-                Poll.create({
-                    id: sentPoll.result.poll.id,
-                    chat_id: sentPoll.result.chat.id,
-                    correct_option: sentPoll.result.poll.correct_option_id,
-                });
-                toReturn.push({ id: chat.id, name: chat.chat, question: sentPoll.result.poll.question });
-            } catch (e) { console.error(e); }
-            i = (i + 1) % questions.length;
-        }
-    }
-    return toReturn;
+	const { Chat, Poll } = db;
+	const hour = new Date().getHours();
+	const registeredChats = await sequelize.query(
+		'SELECT id, chat, active, hours, locale, created_at AS createdAt, updated_at AS updatedAt FROM chats AS Chat WHERE Chat.active = true AND Chat.hours LIKE CONCAT("%$",IF($1+Chat.locale<0, (24+($1+Chat.locale)), (($1+Chat.locale) % 24)),"$%")',
+		{
+			bind: [hour],
+			type: QueryTypes.SELECT,
+			model: Chat,
+			mapToModel: true,
+		}
+	);
+	const res = await fetch(
+		`https://opentdb.com/api.php?amount=${registeredChats.length}&type=multiple&encode=base64`
+	);
+	const toReturn = [];
+	if (res.ok) {
+		const { results: questions } = await res.json();
+		let i = 0;
+		const promises = [];
+		for (let chat of registeredChats) {
+			promises.push(async () => {
+				const data = questions[i];
+				const options = shuffle([
+					...data.incorrect_answers,
+					data.correct_answer,
+				]);
+				const sentPoll = await utils.sendPoll({
+					chat_id: chat.id,
+					question: base64ToString(data.question),
+					options: options.map(base64ToString),
+					is_anonymous: false,
+					type: 'quiz',
+					correct_option_id: options.indexOf(data.correct_answer),
+					open_period: 600,
+				});
+				console.log(sentPoll);
+				try {
+					Poll.create({
+						id: sentPoll.result.poll.id,
+						chat_id: sentPoll.result.chat.id,
+						correct_option: sentPoll.result.poll.correct_option_id,
+					});
+					toReturn.push({
+						id: chat.id,
+						name: chat.chat,
+						question: sentPoll.result.poll.question,
+					});
+				} catch (e) {
+					console.error(e);
+				}
+			});
+			i = (i + 1) % questions.length;
+		}
+	}
+	await Promise.allSettled(promises);
+	return toReturn;
 };
 
 module.exports = {
-    exec,
+	exec,
 };
--- index.js
+++ index.js
@@ -8,77 +8,90 @@ const server = http.createServer(app);
 const db = require('./models');
 
 const answerMiddleware = async (req, res) => {
-    const { body: update } = req;
-    //if theres a new_chat_memberproperty and one of the member is the bot than we need to add the chat
-    //to the db
-    if (update.my_chat_member) {
-        console.log(`Adding to chat ${update.my_chat_member.chat.title}`);
-        const { Chat } = db;
-        try {
-            await Chat.create({
-                id: update.my_chat_member.chat.id,
-                chat: update.my_chat_member.chat.title,
-                active: true,
-            });
-        } catch (e) {
-            console.error(e);
-        }
-        utils.sendMessage({
-            chat_id: update.my_chat_member.chat.id,
-            text: "Hi everyone i'm the @QuizOMaticBot, i'll delight and enlight you all with hourly trivia questions. An admin can set me up with the command /sethours (don't forget to set your locale with the command /setlocale). Let the race begin!"
-        });
-        res.sendStatus(200);
-        return;
-    }
-    //if there's a poll answer i add the new answer
-    if (update.poll_answer) {
-        const { Answer, User, Poll } = db;
-        const { poll_answer } = update;
-        console.log(poll_answer);
-        try {
-            User.upsert({
-                id: poll_answer.user.id,
-                username: poll_answer.user.username ? poll_answer.user.username : `${poll_answer.user.first_name}${poll_answer.user.last_name ? ` ${poll_answer.user.last_name}` : ""}`
-            });
-        } catch (e) { console.error(e); }
-        try {
-            const poll = await Poll.findOne({
-                where: {
-                    id: poll_answer.poll_id,
-                }
-            });
-            Answer.create({
-                poll_id: poll_answer.poll_id,
-                user_id: poll_answer.user.id,
-                chat_id: poll.chat_id,
-                answer: poll_answer.option_ids[0],
-                correct: poll_answer.option_ids[0] === poll.correct_option,
-                time_to_answer: Math.floor((new Date() - poll.createdAt) / 1000),
-            });
-        } catch (e) { console.error(e); }
-        res.sendStatus(200);
-        return;
-    }
-    if (update.callback_query) {
-        const [callback, ...callbackArgs] = update.callback_query.data.split(" ");
-        try {
-            const command = require(`./callbacks/${callback}`);
-            command.exec(update, callbackArgs);
-        } catch (e) { }
-        res.sendStatus(200);
-        return;
-    }
-    const commandString = utils.getCommand(update);
-    if (commandString !== "") {
-        console.log("EXECUTING COMMAND ", commandString);
-        try {
-            const command = require(`./commands${commandString}`);
-            command.exec(update);
-        } catch (e) {
-            console.error(e);
-        }
-    }
-    res.sendStatus(200);
+	const { body: update } = req;
+	//if theres a new_chat_memberproperty and one of the member is the bot than we need to add the chat
+	//to the db
+	if (update.my_chat_member) {
+		console.log(`Adding to chat ${update.my_chat_member.chat.title}`);
+		const { Chat } = db;
+		try {
+			await Chat.create({
+				id: update.my_chat_member.chat.id,
+				chat: update.my_chat_member.chat.title,
+				active: true,
+			});
+		} catch (e) {
+			console.error(e);
+		}
+		utils.sendMessage({
+			chat_id: update.my_chat_member.chat.id,
+			text: "Hi everyone i'm the @QuizOMaticBot, i'll delight and enlight you all with hourly trivia questions. An admin can set me up with the command /sethours (don't forget to set your locale with the command /setlocale). Let the race begin!",
+		});
+		res.sendStatus(200);
+		return;
+	}
+	//if there's a poll answer i add the new answer
+	if (update.poll_answer) {
+		const { Answer, User, Poll } = db;
+		const { poll_answer } = update;
+		console.log(poll_answer);
+		try {
+			User.upsert({
+				id: poll_answer.user.id,
+				username: poll_answer.user.username
+					? poll_answer.user.username
+					: `${poll_answer.user.first_name}${
+							poll_answer.user.last_name
+								? ` ${poll_answer.user.last_name}`
+								: ''
+					  }`,
+			});
+		} catch (e) {
+			console.error(e);
+		}
+		try {
+			const poll = await Poll.findOne({
+				where: {
+					id: poll_answer.poll_id,
+				},
+			});
+			Answer.create({
+				poll_id: poll_answer.poll_id,
+				user_id: poll_answer.user.id,
+				chat_id: poll.chat_id,
+				answer: poll_answer.option_ids[0],
+				correct: poll_answer.option_ids[0] === poll.correct_option,
+				time_to_answer: Math.floor(
+					(new Date() - poll.createdAt) / 1000
+				),
+			});
+		} catch (e) {
+			console.error(e);
+		}
+		res.sendStatus(200);
+		return;
+	}
+	if (update.callback_query) {
+		const [callback, ...callbackArgs] =
+			update.callback_query.data.split(' ');
+		try {
+			const command = require(`./callbacks/${callback}`);
+			command.exec(update, callbackArgs);
+		} catch (e) {}
+		res.sendStatus(200);
+		return;
+	}
+	const commandString = utils.getCommand(update);
+	if (commandString !== '') {
+		console.log('EXECUTING COMMAND ', commandString);
+		try {
+			const command = require(`./commands${commandString}`);
+			command.exec(update);
+		} catch (e) {
+			console.error(e);
+		}
+	}
+	res.sendStatus(200);
 };
 
 //dev only
@@ -95,14 +108,20 @@ const answerMiddleware = async (req, res) => {
 app.post(`/${process.env.BOT_TOKEN}`, answerMiddleware);
 
 app.get('/send-polls', async (req, res) => {
-    try {
-        const command = require(`./cron/send_polls`);
-        const chats = await command.exec();
-        return res.json(chats);
-    } catch (e) { console.error(e); }
-    res.sendStatus(200);
+	try {
+		const command = require(`./cron/send_polls`);
+		const chats = await command.exec();
+		return res.json(chats);
+	} catch (e) {
+		console.error(e);
+	}
+	res.sendStatus(200);
+});
+
+app.get('/hc', async (req, res) => {
+	res.sendStatus(200);
 });
 
 server.listen(process.env.PORT || 3333, () => {
-    console.log("Server up and running...");
+	console.log('Server up and running...');
 });
